3671b1f54097f54bd04e7b13009f2dff26abd665
kritshuang <kritshuang@zego.im>
2023-05-19 11:19:42 +0800

    delete build cache

--- test/assets/test_build_cache/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { spec, it, runTests, beforeAll, afterAll, afterEach, beforeEach } from "./spec-container";
-export { spec, it, runTests, beforeAll, afterAll, afterEach, beforeEach };
--- test/assets/test_build_cache/test/common/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { spec, it, runTests, beforeAll, afterAll, beforeEach, afterEach } from "../../src/spec-container";
-import { config } from "./spec.config";
-import { expect } from "chai";
-export { spec, it, runTests, config, expect, beforeAll, afterAll, beforeEach, afterEach };
